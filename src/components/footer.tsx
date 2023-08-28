c0891569c9a7156f7901a7bd11545e2aafcbf373
Artur <admin@deswolf.com>
2023-08-28 13:33:47 +0100

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,10 @@
 
 export const Footer = () => {
   return (
-    <footer className="py-6 md:px-8 md:py-0">
+    <footer className="py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          &copy; 2023 | Built by dw_
-          {/* <a href="https://twitter.com/shadcn" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">shadcn</a>. */}
+        <p className="px-0 sm:px-4 text-center text-xs leading-loose text-muted-foreground md:text-left">
+          Built by dw_ for personal non-commercial use only. Data provided by Sportlabs.
         </p>
       </div>
     </footer>
